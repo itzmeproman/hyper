c9bcc89618d99b57b009362783826e445e4c3410
Welexa <157591305+Welexa@users.noreply.github.com>
2024-03-12 17:48:48 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -86,7 +86,7 @@ async def start_command(client: Client, message: Message):
             InlineKeyboardButton("ʜᴇʟᴘ​", callback_data="help")
         ],
         [
-            InlineKeyboardButton("ᴄʜᴀɴɴᴇʟ", url="https://t.me/AnimeX_Hyper"),
+            InlineKeyboardButton("ᴄʜᴀɴɴᴇʟ", url="https://t.me/AnimeX_Hindii"),
             InlineKeyboardButton("ᴏɴɢᴏɪɴɢ", url="https://t.me/Ongoing_Hyper")
         ],
         [
